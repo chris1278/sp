72dc2447ed8d91615779d4151a0b8543886df616
chris1278 <31364765+chris1278@users.noreply.github.com>
2020-04-21 19:07:28 +0200

    Add files via upload

---
+++ chris1278/sp/language/en/sp.php
@@ -0,0 +1,22 @@
+<?php
+/**
+*
+* @package phpBB Extension - 
+* @copyright (c) 2017 ()
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'MINIPOM'				=> '*Minipom is the registered trademark of THE COCA-COLA COMPANY (n.d.Ges.d. Staates Delaware), Atlanta Ga., US',
+
+));
